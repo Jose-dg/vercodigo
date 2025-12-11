1f7e0276e76fc2a13d6fe06558fc123ecffee7f3
José Ojeda <jose.ojeda.k@hotmail.com>
2025-12-11 12:25:26 -0500

    New visual 2.0

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -35,7 +35,6 @@ export default function LoginPage() {
                 });
             } else {
                 router.push("/store");
-                router.refresh();
             }
         } catch (error) {
             toast({
